c9e61ca9fd3577a44b02515ac086e15844cf4752
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-07-01 11:12:02 +0530

    1- project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "u hit signup end",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    data: "u hit login end",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "u hit logout end",
+  });
+};
