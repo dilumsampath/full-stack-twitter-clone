a87088932400daa3071cd0a5da14927b6ddceb83
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-07-04 12:02:59 +0530

    1- post routes and controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -93,7 +93,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.cookie("jwt", { maxAge: 0 });
+    res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
---
+++ backend/routes/post.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:userName", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
