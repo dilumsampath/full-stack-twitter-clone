6f57c833f4e05176128c738a8b534a3d1c157d18
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-07-07 18:17:09 +0530

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
