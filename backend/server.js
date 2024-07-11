0dd4f2ada2247ee9ce3e7ff5cbd7d87ab78d8fda
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-07-11 16:05:35 +0530

    6- update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,8 @@ app.use("/api/notifications", notificationRoutes);
 //   });
 // }
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
